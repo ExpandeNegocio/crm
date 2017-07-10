a77eb05e9c103c0e56f00ba28974739b634f1fd6
itcexpande <itc@expandenegocio.com>
2017-07-10 19:50:49 +0200

    Cambio de nomenclaturas

--- custom/modules/Expan_Franquicia/language/es_ES.lang.php
+++ custom/modules/Expan_Franquicia/language/es_ES.lang.php
@@ -1,12 +1,12 @@
 <?php
 // created: 2014-11-10 20:14:32
 $mod_strings = array (
-  'LBL_ASSIGNED_TO_NAME' => 'Director de Expansion:',
-  'LBL_ASSIGNED_USER' => 'Director de Expansión:',
+  'LBL_ASSIGNED_TO_NAME' => 'Responsable de Expansion:',
+  'LBL_ASSIGNED_USER' => 'Responsable de Expansión:',
   'LBL_GESTIONADO_POR' => 'Gestionado por:',
   'LBL_FILTRO_SOLICITUDES_USER_ID' => 'Filtro de solicitudes (relacionado Usuario ID)',
   'LBL_FILTRO_SOLICITUDES' => 'Filtro de solicitudes',
-  'LBL_DIRECTOR_CONSULTORIA'=>'Director de Consultoria',
+  'LBL_DIRECTOR_CONSULTORIA'=>'Director de cuenta',
   'LBL_MARCAR_PAGADO_BUTTON'=>'Marcar Pagado',
   'LBL_ FILTRO_SOLICITUDES'=>'Filtro de solicitudes',
 );
