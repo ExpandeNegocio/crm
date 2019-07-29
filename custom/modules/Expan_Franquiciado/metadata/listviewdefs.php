c973e3ec4a0ebf0cfb27856aed80a23609ae0343
itcexpande <itc@expandenegocio.com>
2019-07-29 17:11:41 +0200

    #5662 - [FRANQUICIADO]Check franquiciado Activo - Se han añadido los filtro

--- custom/modules/Expan_Franquiciado/metadata/listviewdefs.php
+++ custom/modules/Expan_Franquiciado/metadata/listviewdefs.php
@@ -1,38 +1,44 @@
 <?php
 $module_name = 'Expan_Franquiciado';
-$listViewDefs [$module_name] = 
-array (
+$listViewDefs [$module_name] =
+  array(
 
- 'NAME' => 
-  array (
-    'width' => '20%',
-    'label' => 'LBL_NAME',
-    'link' => true,
-    'orderBy' => 'last_name',
-    'default' => true,
-    'related_fields' => 
-    array (
-      0 => 'first_name',
-      1 => 'last_name',
-      2 => 'salutation',
-    ),
-  ),
-  
-  'PHONE_MOBILE' => 
-  array (
-    'width' => '10%',
-    'label' => 'LBL_MOBILE_PHONE',
-    'default' => true,
-  ),
-  'EMAIL1' => 
-  array (
-    'width' => '15%',
-    'label' => 'LBL_EMAIL_ADDRESS',
-    'sortable' => false,
-    'link' => true,
-    'customCode' => '{$EMAIL1_LINK}{$EMAIL1}</a>',
-    'default' => true,
-  ),
-  
-);
+    'NAME' =>
+      array(
+        'width' => '20%',
+        'label' => 'LBL_NAME',
+        'link' => true,
+        'orderBy' => 'last_name',
+        'default' => true,
+        'related_fields' =>
+          array(
+            0 => 'first_name',
+            1 => 'last_name',
+            2 => 'salutation',
+          ),
+      ),
+
+    'estado' =>
+      array(
+        'width' => '10%',
+        'label' => 'LBL_ESTADO',
+        'default' => true,
+      ),
+
+    'PHONE_MOBILE' =>
+      array(
+        'width' => '10%',
+        'label' => 'LBL_MOBILE_PHONE',
+        'default' => true,
+      ),
+    'EMAIL1' =>
+      array(
+        'width' => '15%',
+        'label' => 'LBL_EMAIL_ADDRESS',
+        'sortable' => false,
+        'link' => true,
+        'customCode' => '{$EMAIL1_LINK}{$EMAIL1}</a>',
+        'default' => true,
+      ),
+  );
 ?>
