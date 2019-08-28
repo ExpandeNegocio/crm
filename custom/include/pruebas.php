2b37c6d03c4e0bd537cc54ad48b6fa7a38bb99b8
itcexpande <itc@expandenegocio.com>
2019-08-28 08:06:34 +0200

    #7923 Se ha creado un script para calcularlo

--- custom/include/pruebas.php
+++ custom/include/pruebas.php
@@ -1,19 +1,19 @@
 <?php
+require_once ('modules/Expan_Franquiciado/Expan_Franquiciado.php');
+
 $db = DBManagerFactory::getInstance();
 
-
-
-$sql = "SELECT id as idF FROM expan_franquiciado f, ";
-$sql = $sql . " (select phone_home as phf, phone_mobile as pmf from expan_solicitud where id='e169842f-3481-1f9c-0d37-5d64dd7f207e') b ";
-$sql = $sql . " WHERE  (phone_home =phf OR phone_home=pmf OR phone_mobile =phf OR phone_mobile=pmf) AND deleted=0;";
-
-echo $sql."<BR>";
+$sql = "SELECT id as idF FROM expan_franquiciado f ";
 
 $GLOBALS['log'] -> info('[ExpandeNegocio][Creacion Franquiciado] Consulta'.$sql);
 
 $result = $db -> query($sql, true);
 
 while ($row = $db -> fetchByAssoc($result)) {
-  $GLOBALS['log'] -> info('[ExpandeNegocio][Creacion Franquiciado] Entra Bucle-'.$row["idF"]);
-  echo $row["idF"];
+  echo $row["idF"]."-Actualizado<BR>";
+
+  $expan_franquiciado= new Expan_Franquiciado();
+  $expan_franquiciado->retrieve($row["idF"]);
+  $expan_franquiciado ->estado= $expan_franquiciado->getEstado();
+  $expan_franquiciado->save();
 }
