58b1535356043aabff0865fba62ca5ab345ab39d
itcexpande <itc@expandenegocio.com>
2019-12-02 17:48:39 +0100

    #7986 Cambio en la forma de calculo de tags

--- custom/include/procesarTags.php
+++ custom/include/procesarTags.php
@@ -3,35 +3,14 @@
     require_once ('modules/Expan_Solicitud/Expan_Solicitud.php');
     
     $db = DBManagerFactory::getInstance();
-    
-    $query = "select tag from expan_tag_perfil";
-    
-    $result = $db -> query($query, true);
-    
-    //Recorremos los usuarios
-    while ($row = $db -> fetchByAssoc($result)) {
-    
-        $tag = $row["tag"];
-        $query2 = "select id from expan_solicitud where perfil_profesional like '%" . $tag . "%'; ";
-    
-        $result2 = $db -> query($query2, true);
-    
-        while ($row2 = $db -> fetchByAssoc($result2)) {
-    
-            $solicitud = new Expan_Solicitud();
-            $solicitud -> retrieve($row2["id"]);
-    
-            $pakTag = "^" . $tag . "^";
-    
-            $pos = strpos($solicitud -> tags_empresa, $pakTag);
-    
-            if ($pos === false) {
-                if ($solicitud -> tags_empresa == "" || $solicitud -> tags_empresa === null) {
-                    $solicitud -> tags_empresa = $pakTag;
-                } else {
-                    $solicitud -> tags_empresa = $solicitud -> tags_empresa . "," . $pakTag;
-                }
-            }
-        }
-    }
+
+    $query = "update expan_solicitud s join ( ";
+    $query=$query."select distinct(s.id) id, s.perfil_profesional , tipoTag from expan_solicitud s, expan_tag_perfil t ";
+    $query=$query."where s.perfil_profesional like concat('%',t.Tag,'%') ";
+    $query=$query."and     instr(coalesce(tags_empresa,''),t.tipoTag)=0)a ";
+    $query=$query."on s.id=a.id ";
+    $query=$query."set s.tags_empresa=case when s.tags_empresa is null then a.tipoTag else concat(s.tags_empresa,',',tipoTag) end ";
+
+    $result = $db -> query($query);;
+
 ?>
